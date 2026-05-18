ec46a93f7a5221579efbbd4538f1e0b972719565
SurajJaybhaye <101545931+SurajJaybhaye@users.noreply.github.com>
2026-05-18 10:35:28 +0530

    Solve cors issue

--- server/server.js
+++ server/server.js
@@ -1,28 +1,47 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { clerkMiddleware, requireAuth } from '@clerk/express'
+import { clerkMiddleware, requireAuth } from '@clerk/express';
 import aiRouter from './routes/aiRoutes.js';
 import connectCloudinary from './configs/cloudinary.js';
 import userRouter from './routes/userRoutes.js';
 
-const app = express()
+const app = express();
 
-await connectCloudinary()
+await connectCloudinary();
 
-app.use(cors())
-app.use(express.json())
-app.use(clerkMiddleware())
+app.use(
+    cors({
+        origin: [
+            'https://quick-ai-front-pink.vercel.app',
+            'http://localhost:5173'
+        ],
+        methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+        credentials: true,
+        allowedHeaders: [
+            'Content-Type',
+            'Authorization'
+        ]
+    })
+);
 
-app.get('/', (req, res)=>res.send('Server is Live!'))
+app.options('*', cors());
 
-app.use(requireAuth())
+app.use(express.json());
 
-app.use('/api/ai', aiRouter)
-app.use('/api/user', userRouter)
+app.use(clerkMiddleware());
+
+app.get('/', (req,res)=>{
+    res.send('Server is Live!');
+});
+
+app.use(requireAuth());
+
+app.use('/api/ai', aiRouter);
+app.use('/api/user', userRouter);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
-    console.log('Server is running on port', PORT);
-})
+    console.log(`Server running on ${PORT}`);
+});
